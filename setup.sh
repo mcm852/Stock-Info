2dbf9fe3ec8d509535b26b477b33875dd4aeda26
mcm852 <matt.mariutto@gmail.com>
2018-05-20 11:41:20 -0500

    Can search for stocks by name

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-echo -e '.separator "," \n.import src/main/resources/NYSE.csv nyse' | sqlite3 src/main/resources/stocks.db;
-echo -e '.separator "," \n.import src/main/resources/NASDAQ.csv nasdaq' | sqlite3 src/main/resources/stocks.db;
-echo -e '.separator "," \n.import src/main/resources/AMEX.csv amex' | sqlite3 src/main/resources/stocks.db;
+echo -e '.separator "," \n.import src/main/resources/NYSE.csv stocks' | sqlite3 src/main/resources/stocks.db;
+echo -e '.separator "," \n.import src/main/resources/NASDAQ.csv stocks' | sqlite3 src/main/resources/stocks.db;
+echo -e '.separator "," \n.import src/main/resources/AMEX.csv stocks' | sqlite3 src/main/resources/stocks.db;
--- src/main/java/com/matt/stockinfo/service/StockService.java
+++ src/main/java/com/matt/stockinfo/service/StockService.java
@@ -1,7 +1,15 @@
 package com.matt.stockinfo.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,9 +18,13 @@ public class StockService {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 	
-//	@RequestMapping(name = "/symbols", method = RequestMethod.GET)
-//	ResponseEntity<?> getSymbols(@RequestParam(value="name", required=true) String name) {
-//		
-//	}
+	@RequestMapping(name = "/symbols", method = RequestMethod.GET)
+	ResponseEntity<?> getSymbols(@RequestParam(value="name", required=true) String name) {
+		
+		List<Map<String,Object>> res = jdbcTemplate.queryForList("SELECT * FROM stocks "
+				+ "where Name like '" + name + "%';");
+		
+		return new ResponseEntity<>(res, HttpStatus.OK);
+	}
 
 }
